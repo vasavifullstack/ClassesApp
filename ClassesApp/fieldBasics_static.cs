02b43b7988dc6fb1fce29b113945211f98f2c558
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-18 22:14:46 +0530

    Add project files.

---
+++ ClassesApp/fieldBasics_static.cs
@@ -0,0 +1,28 @@
+﻿using System;
+ class FieldBasics
+    {
+    //Fields are nothting but kind of variables which can hold the values. Value can be anything.
+    //static members
+    static string filename = "vasavi_resume.pdf";
+    static string fileLocation = @"D:\Students\Resume";
+    static string fileSize = "3kb";
+    static string filecreatedDate = "Mar-01-2025";
+
+    static DateTime applyResumeDateAndTime = DateTime.Now;
+
+
+
+    static void Main() 
+    
+    { 
+       
+        string studentfilename=FieldBasics.filename;
+        Console.WriteLine(studentfilename);
+
+        DateTime date = FieldBasics.applyResumeDateAndTime;
+        Console.WriteLine(date);
+
+
+
+        }
+    }
