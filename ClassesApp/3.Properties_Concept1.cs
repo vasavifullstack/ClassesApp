2122d1dc17e24c2b38c7bc828dce3597f111ecfc
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-20 18:53:47 +0530

    property

---
+++ ClassesApp/3.Properties_Concept1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Student
+{
+    private string name; // private field
+
+    public string Name   // Property
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+
+    static void Main()
+    {
+        Student s = new Student();
+        s.Name = "John";      // set
+        Console.WriteLine(s.Name);   // get
+    }
+}
---
+++ ClassesApp/4.properties2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+     class Employ_details
+    {
+
+    //here "_" underscore refers field-not having control of values.so cannot protect the data
+
+    //string _employ_name = "vasavi reddy";
+    //property always start with uppercase to control values get and set method.
+    //to protect the filed .we need to use property.
+    string Employ_name { get; set; }
+
+    string Employ_ID { get; }
+
+    string Salary { get; }
+
+    string Employ_Location { get; set; }
+
+
+
+    static void Main()
+    {
+        Employ_details details = new Employ_details();
+        //both get and set the values
+        details.Employ_name = "vasavi reddy";
+        Console.WriteLine(details);
+        //only get the value .cannot modify.
+        details.Employ_ID.ToString();
+
+        details.Employ_Location.ToString();
+
+        details.Salary.ToString();
+
+
+    }
+    }
