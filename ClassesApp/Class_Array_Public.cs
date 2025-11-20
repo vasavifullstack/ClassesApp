ce8f81995408eecfcc5222a183fa6bfbfa29c934
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-20 19:59:53 +0530

    constructor

---
+++ ClassesApp/Class_Array_Public.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class File1
+{
+    //string _filename;
+    //string _filelocation;
+    //string _filesize;
+    //string _createddate;
+
+    public string FileName { get; set; }
+    public string FileLocation { get; set; }
+    public string FileSize { get; set; }
+    public string CreatedDate { get; set; }
+}
+class File2
+    {
+        public void method1(string[] fileNames)
+        {
+
+        }
+    }
+    class File3
+    {
+        static void Main()
+        {
+            File2 data = new File2();
+            string[] fileNames = { "PDFFile1.pdf", "PDFFile2.pdf", "PDFFile3.pdf" };
+            data.method1(fileNames);
+            Console.WriteLine(fileNames);
+        }
+    }
---
+++ ClassesApp/Constructor.cs
@@ -0,0 +1,44 @@
+﻿
+//constructor -is a special method inside of a class which is automatically executes
+    //once you create the instance.
+
+    //constructor name and class name should be same.
+    //constructor is method can accept the parameter
+using System;
+class FileDetailsConstructors
+{
+
+    string filename = "Student1_Resume.docx";
+    string filelocation = @"D:\Students\Resumes";
+    string filesize = "2Kb";
+    string createddate = "Mar-01-2024";
+    DateTime createdDateTime = DateTime.Now;
+
+
+
+    // pass the multi parameters to the constuctor and check.
+    public FileDetailsConstructors(string resumeName)
+    {
+        ///Code here..
+        ///
+        Console.WriteLine("File object created");
+        Console.WriteLine(this.filename); // "Student1_Resume.docx"
+
+        //this.filename = "Sam_reusme.docx";
+        this.filename = resumeName;
+
+    }
+
+    static void Main()
+    {
+        FileDetailsConstructors file1 = new FileDetailsConstructors("Studnet2_resume.docx");
+
+        //Note : i have changed the filename inside of the constrcutor.
+
+        Console.WriteLine(file1.filename);  //Sam_reusme.docx
+    }
+
+
+
+
+}
