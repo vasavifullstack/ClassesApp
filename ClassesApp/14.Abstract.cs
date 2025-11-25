46897b362ee742c9ecbe2f6f79164035cff557e4
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-25 21:23:58 +0530

    compil_polymerism

---
+++ ClassesApp/14.Abstract.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+//An abstract class:
+
+// Cannot be instantiated
+// Can contain abstract methods(no body)
+//Can contain concrete methods (with body)
+// Can contain fields, constructors, properties
+//Every child class MUST give its own implementation
+// Overrides all abstract methods
+// No extra methods
+//You use the abstract class reference to store child class objects.
---
+++ ClassesApp/15.compiler_polymorism.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ClassesApp
+{
+    //method overloading -- compile polymorphisam
+    class SalaryCalculator
+    {
+
+        //static void Main()
+        //{
+
+        //}
+        public double CalculateSalary(double monthlySalary)
+        {
+            return monthlySalary;
+        }
+        public double CalculateSalary(int monthlySalary)
+        {
+            return monthlySalary;
+        }
+        public double CalculateSalary(int hoursworked, int hoursRate)
+        {
+            return hoursworked * hoursRate;
+        }
+        public double CalculateSalary(double salesAmount, double commissionRate)
+        {
+            return salesAmount * commissionRate;
+        }
+        public double CalculateSalary(int hoursWorked, int hourlyRate, double bonus)
+        {
+            return (hoursWorked * hourlyRate) + bonus;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            SalaryCalculator salaryCalculator = new SalaryCalculator();
+            //salaryCalculator.CalculateSalary()
+
+            double salary1 = salaryCalculator.CalculateSalary(10000.09);
+            double salary2 = salaryCalculator.CalculateSalary(10000);
+            double salary3 = salaryCalculator.CalculateSalary(9, 4);
+            double salary4 = salaryCalculator.CalculateSalary(5000, 0.1);
+            double salary5 = salaryCalculator.CalculateSalary(9, 4, 100);
+
+            Console.WriteLine($"Salary 1: {salary1}"); // Output: Salary 1: 10000.09
+            Console.WriteLine($"Salary 2: {salary2}"); // Output: Salary 1: 
+            Console.WriteLine($"Salary 3: {salary3}"); // Output: Salary 1: 
+            Console.WriteLine($"Salary 4: {salary4}"); // Output: Salary 1: 
+            Console.WriteLine($"Salary 5: {salary5}"); // Output: Salary 1: 
+
+
+
+        }
+    }
+
+}
