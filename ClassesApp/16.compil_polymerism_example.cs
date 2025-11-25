7de714c87f7ffad293dcf99c38105630492d4fc7
vasavifullstack <vasavi.fullstack@gmail.com>
2025-11-25 21:46:49 +0530

    example

---
+++ ClassesApp/16.compil_polymerism_example.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ class Addition
+    {
+    public int add(int first, int second) {
+        return first + second;
+    }
+    public int add(int first, int second,int third)
+    {
+        return first + second + third;
+    }
+    
+    class Progarm { }
+    static void Main(string[] args)
+    { 
+        Addition result = new Addition();
+        
+        Console.WriteLine(result.add(10, 20));
+        Console.WriteLine(result.add(30,50,35));
+      
+
+    }
+}
